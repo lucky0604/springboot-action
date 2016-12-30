ccd2a48be505994be16db98774fc580fa91ea343
Lucky <175917645@qq.com>
2016-12-30 18:25:30 +0800

    spring mvc 4.x

--- src/main/java/com/lucky/ch03/annotation/NewConfig.java
+++
@@ -1,9 +0,0 @@
-package com.lucky.ch03.annotation;
-
-/**
- * Created by lucky on 12/30/16.
- */
-// 使用@WiselyConfiguration组合注解替代@Configuration何@ComponentScan
-@WiselyConfiguration("com.lucky.ch03.annotation")
-public class NewConfig {
-}
