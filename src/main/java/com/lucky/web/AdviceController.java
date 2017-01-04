175051cf279780e1999bfb4f54091cc0375718d8
Lucky <175917645@qq.com>
2017-01-04 23:13:03 +0800

    add exception advice view

---
+++ src/main/java/com/lucky/web/AdviceController.java
@@ -0,0 +1,17 @@
+package com.lucky.web;
+
+import com.lucky.domain.DemoObj;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lucky on 17-1-4.
+ */
+@Controller
+public class AdviceController {
+    @RequestMapping("/advice")
+    public String getSomething(@ModelAttribute("msg") String msg, DemoObj obj) {
+        throw new IllegalArgumentException("非常抱歉，参数有误/" + "来自@ModelAttribute:" + msg);
+    }
+}
---
+++ src/main/resources/views/error.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE>
+<html>
+<head>
+    <title>@ControllerAdvice Demo</title>
+</head>
+<body>
+${errorMessage}
+</body>
+</html>
