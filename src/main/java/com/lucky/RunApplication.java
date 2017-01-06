8caccb3164f74318b355a420e8995ebbf79d37e9
Lucky <175917645@qq.com>
2017-01-06 18:34:15 +0800

    merge thymeleaf template engine

--- src/main/java/com/lucky/RunApplication.java
+++ src/main/java/com/lucky/RunApplication.java
@@ -1,17 +1,23 @@
 package com.lucky;
 
 import com.lucky.config.AuthorSettings;
+import com.lucky.domain.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lucky on 1/6/17.
  */
-@RestController
+@Controller
 @SpringBootApplication
 public class RunApplication {
 
@@ -27,6 +33,7 @@ public class RunApplication {
      * 类型安全的配置
      * @return
      */
+    /*
     @Autowired
     private AuthorSettings authorSettings;
 
@@ -35,6 +42,27 @@ public class RunApplication {
         // return "book name is: " + bookName + " and book author is : " + bookAuthor;
         return "author name is " + authorSettings.getName() + " and author age is " + authorSettings.getAge();
     }
+    */
+
+    /**
+     * 利用thymeleaf获取数据
+     * @param args
+     */
+    @RequestMapping("/")
+    public String index(Model model) {
+        Person single = new Person("aa", 11);
+        List<Person> people = new ArrayList<Person>();
+        Person p1 = new Person("xx", 11);
+        Person p2 = new Person("yy", 22);
+        Person p3 = new Person("zz", 33);
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        model.addAttribute("singlePerson", single);
+        model.addAttribute("people", people);
+        return "index";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(RunApplication.class, args);
     }
---
+++ src/main/java/com/lucky/domain/Person.java
@@ -0,0 +1,35 @@
+package com.lucky.domain;
+
+/**
+ * Created by lucky on 1/6/17.
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person() {
+        super();
+    }
+
+    public Person(String name, Integer age) {
+        super();
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
